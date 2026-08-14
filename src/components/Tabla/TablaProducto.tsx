b124205a706345253d4b6d813aa8e7d1ea0101ae
Enzo887 <enzoduarte2014@gmail.com>
2026-08-14 20:02:13 -0300

    chore: empece a migrar el formato de la tabla productos

--- src/components/Tabla/TablaProducto.tsx
+++ src/components/Tabla/TablaProducto.tsx
@@ -1,3 +1,4 @@
+import { Container, Row, Button, Table} from 'react-bootstrap'
 import {type Producto} from '../../types/producto.types'
 import { RenglonProducto } from './RenglonProducto'
 
@@ -8,18 +9,29 @@ interface TablaProductoProps{
 
 export function TablaProducto({productos}: TablaProductoProps){
     return(
-        <table>
-            <thead>
-                <tr>
-                    <th>Nombre</th>
-                    <th>Precio</th>   
-                </tr>
-            </thead>
-            <tbody>
-                {productos.map(producto => (
-                    <RenglonProducto key={producto.id} producto={producto} />
-                ))}
-            </tbody>
-        </table>
+        <Container fluid className='py-3'>
+            <Row className='justify-content-center'>
+                <div className='col-12 col-lg-10 col-xl-8'>
+                    <div className='mb-3 d-flex justify-content-end'>
+                        <Button variant='success'/>
+                    </div>
+                </div>
+                <Table striped bordered hover>
+                    <thead className="table-primary">
+                        <tr className="text-center align-middle">
+                            <th scope="col" className="fw-bold text-aling-center">Producto</th>
+                            <th scope="col" className="fw-bold">Precio Actual<small className="text-muted"></small></th>
+                            <th scope="col" className="fw-bold">Editar</th>
+                            <th scope="col" className="fw-bold">Eliminar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {productos.map(producto => (
+                            <RenglonProducto key={producto.id} producto={producto} />
+                        ))}
+                    </tbody>
+                </Table>
+            </Row>
+        </Container>
     )
 }
